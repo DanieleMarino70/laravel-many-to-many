ff9888f06b546ceb8d26011f14cb3d843677baba
DanieleMarino70 <danielaotitan@gmail.com>
2023-04-19 21:50:54 +0200

    add: possibility to project type to be null

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -18,6 +18,7 @@ class ProjectSeeder extends Seeder
     public function run(Faker $faker)
     {
         $types = Type::all()->pluck('id');
+        $types[] = null;
         for ($i = 0; $i < 10; $i++) {
             $project = new Project;
             $project->type_id = $faker->randomElement($types);
