0a88546372c5d7c9213183878b4dc4f42e1999cf
DanieleMarino70 <danielaotitan@gmail.com>
2023-04-19 19:52:00 +0200

    add: types route to navbar partial

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -39,6 +39,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('projects.index') }}">{{ __('Projects') }}</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('types.index') }}">{{ __('Types') }}</a>
+                        </li>
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }}
